5fe94c32dba3e356db0f5babf7378fb43c348334
nemusheep <miyomayo02@gmail.com>
2025-07-10 09:38:14 +0900

    install gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https:itsujiht.github.io/puzzlePanel/',
+  base: '/puzzlePanel/',
   plugins: [react()],
 })
